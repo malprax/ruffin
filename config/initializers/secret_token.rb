502034acbf08a4dfbd62b84c702c2dfb66368db8
aulia <kingmalprax@gmail.com>
2013-09-11 13:46:19 +0800

    try rails 4

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Ruffin::Application.config.secret_token = 'fe273445a4f8ef72f0a14e15f6a198b98f135e2d2737940097bfaaf82c6e2476ab6cdd8410b8b6e44a9d0e4fc22092b9801a35d6972889609249bba94c07edd3'
+Ruffin::Application.config.secret_token = 'xxfe273445a4f8ef72f0a14e15f6a198b98f135e2d2737940097bfaaf82c6e2476ab6cdd8410b8b6e44a9d0e4fc22092b9801a35d6972889609249bba94c07edd3'
